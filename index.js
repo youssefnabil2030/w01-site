4bfeeedcc944a1b29dfc89855a35db3c68932cdb
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-03-13 16:24:37 -0700

    Refactor static file serving and index route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(".")); // يخلي السيرفر يفتح ملفات HTML
+app.use(express.static(path.join(__dirname))); // serve all files
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html")); // send index.html
+});
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
