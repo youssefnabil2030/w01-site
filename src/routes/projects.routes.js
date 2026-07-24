cb40621dff2d3b683799b27072e7b2f3b55c5cfb
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-24 12:26:32 -0700

    Add routes for projects and project details

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,1 +1,9 @@
+import { Router } from 'express';
+import { getProjectsPage, getProjectDetails } from '../controllers/projects.controller.js';
 
+const router = Router();
+
+router.get('/', getProjectsPage);
+router.get('/project/:id', getProjectDetails);
+
+export default router;
