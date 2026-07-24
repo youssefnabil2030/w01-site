713a7ea9d705c5a457e225f90afcde1d9c9b3f80
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-24 10:23:52 -0700

    Refactor Organization model to use named function

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,16 +1,16 @@
-import db from '../db.js'; // Go up one directory level
+import db from '../db.js';
 
-const Organization = {};
-
-Organization.getAll = async function() {
+const getAllOrganizations = async () => {
   try {
     const sql = "SELECT * FROM public.organizations ORDER BY name ASC";
     const result = await db.query(sql);
     return result.rows;
   } catch (error) {
-    console.error("Error inside Organization.getAll model: ", error);
+    console.error("Error inside getAllOrganizations model: ", error);
     throw error;
   }
 };
 
-export default Organization;
+export default {
+  getAllOrganizations
+};
