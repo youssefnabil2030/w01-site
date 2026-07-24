3a08963301e8d07a06212ae0624b2307044b9f0b
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-24 12:23:56 -0700

    Set up main router with category, project, and organization routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,12 @@
+import { Router } from 'express';
+import categoryRoutes from './categories.route.js';
+import projectRoutes from './projects.route.js';
+import organizationRoutes from './organizations.route.js';
 
+const router = Router();
+
+router.use(categoryRoutes);
+router.use(projectRoutes);
+router.use(organizationRoutes);
+
+export default router;
