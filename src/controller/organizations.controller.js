453713672c8b06989ec9819b768c8c8b2a15fdc0
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-24 12:22:58 -0700

    Update organizations.controller.js

--- src/controller/organizations.controller.js
+++ src/controller/organizations.controller.js
@@ -1,1 +1,38 @@
+import orgModel from '../models/organizations.js';
 
+// عرض صفحة كل المؤسسات
+export const getOrganizationsPage = async (req, res, next) => {
+  try {
+    const organizations = await orgModel.getAllOrganizations();
+    res.render('organizations', {
+      title: 'Organizations',
+      organizations
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// عرض صفحة تفاصيل مؤسسة معينة مع مشاريعها
+export const getOrganizationDetails = async (req, res, next) => {
+  try {
+    const orgId = req.params.id;
+    const organization = await orgModel.getOrganizationById(orgId);
+
+    if (!organization) {
+      const err = new Error('Organization not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    const projects = await orgModel.getProjectsByOrganization(orgId);
+
+    res.render('organization-details', {
+      title: organization.name,
+      organization,
+      projects
+    });
+  } catch (error) {
+    next(error);
+  }
+};
